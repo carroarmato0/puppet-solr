dee767402e8bf9abbb9db17f8ecae2f31c7c5c8a
Christophe Vanlancker <christophe.vanlancker@inuits.eu>
2017-06-12 16:42:48 +0200

    Make check for webinstaller more robuts and ensure unzip is installed before the extraction
    Signed-off-by: Christophe Vanlancker <christophe.vanlancker@inuits.eu>

--- manifests/installer/web.pp
+++ manifests/installer/web.pp
@@ -1,12 +1,19 @@
 # Handles the installation of Solr6 from the web
 class solr6::installer::web {
 
+  if !defined(Package['unzip']) {
+    package{'unzip':
+      ensure => present,
+      before => Archive["/tmp/solr-${::solr6::version}.zip"],
+    }
+  }
+
   archive { "/tmp/solr-${::solr6::version}.zip":
     ensure       => present,
     extract      => true,
     extract_path => '/opt/',
     source       => "${::solr6::params::download_url_base}/${::solr6::version}/solr-${::solr6::version}.zip",
-    creates      => "/opt/solr-${::solr6::version}",
+    creates      => "/opt/solr-${::solr6::version}/bin/solr",
     cleanup      => true,
   }
 
